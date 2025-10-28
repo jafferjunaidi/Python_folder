50e0fdf44a2f94be0316712446f4ea1e42e0521f
Jaffer Junaidi <jafferjunaidi@outlook.com>
2025-10-29 03:38:06 +0500

    variables

---
+++ variables.py
@@ -0,0 +1,35 @@
+# print method
+print("Hello World")
+
+# string to string concatenation
+print("We are learning " + "Python" )
+
+# string to other datatype concatenation
+print("My age is" , 23)
+
+#variables
+num=13
+print("Number is",num)
+
+# multiple variables store one value
+num1=num2=num3=27
+print(num1,num2,num3)
+
+# multiple variables with multiple values
+num4,num5,num6=35,"Hamza","Haider"
+print(num4,num5,num6)
+
+# single variable with multiple values
+colors=["red","blue","green"]
+a,b,c=colors
+print(a,b,c)
+
+# type casting
+num7=43
+num8=str(17.4)
+print(type(num7))
+print(type(num8))
+
+
+
+
