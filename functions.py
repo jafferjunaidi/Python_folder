2868a833d26d743b6ea5a26ec31609386bc27d37
Jaffer Junaidi <jafferjunaidi@outlook.com>
2025-10-29 03:19:35 +0500

    functions

---
+++ functions.py
@@ -0,0 +1,26 @@
+# userdefined normal function
+# def Stdname(): 
+#     print("My name is Jaffer Junaidi.")
+# Stdname()
+
+def Details():
+    print("Education: B.Sc in Computer Science", "Occupation: Software Engineer")
+# Details()
+# # Details()
+# Details()   # Calling the function multiple times
+# Details()
+
+# userdefined function with parameters
+def Sum(a,b):
+    print(f"Sum of {a} and {b} is {a+b}")
+# Sum(15,25)
+
+# def Employee(name,age):
+#     print(f"Employee is {name} and age is {age}")
+# Employee("Fahad",22)
+
+# def Add(x,y):
+#     return(x+y) # This function takes two parameters x and y and returns their sum
+#     # return x-y  
+# print(Add(10,20)) 
+
