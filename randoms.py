90509f52b588d08ad5368477dfa6245913691661
Jaffer Junaidi <jafferjunaidi@outlook.com>
2025-10-29 03:33:45 +0500

    randoms

---
+++ randoms.py
@@ -0,0 +1,18 @@
+# import random
+# a=random.randint(4,10)
+# print(a) # print a random integer between 4 and 10
+
+# b=random.randrange(1,20) 
+# print(b) # print a random integer between 1 and 20, excluding 20
+
+# val=["apple", "banana", "cherry", "orange", "kiwi"]
+# print(val[2]) # print the third element in the list
+# random.shuffle(val) # shuffle the list   
+# print(val) # print the shuffled list
+# print(random.choice(val)) # randomly select an element from the list
+
+# numlist=[1,3,5,7,9,11]
+# print(random.choice(numlist)) # randomly select an element from the list
+
+# import datetime
+# print(datetime.datetime.now()) # print the current date and time
