1c88d4866537870027793070dd17d65b2ba339ed
Jaffer Junaidi <jafferjunaidi@outlook.com>
2025-10-29 03:22:19 +0500

    loops

---
+++ loops.py
@@ -0,0 +1,48 @@
+# Loops
+
+# range(3)
+# starting from 0, ending at 3
+# increment by 1
+
+# range(4,10)
+# starting at 4, ending at 10
+# increment by 1 
+
+# range(5,20,2)
+# starting at 5, ending at 20
+# increment by 2
+
+# for loop
+
+# range(4)
+# for a in range(4):
+#     print(a)
+    # print(a,"hello World")
+
+# range(4, 10)
+# for b in range(4, 10):
+#     print(b)
+
+# range(5, 20, 2)
+# for c in range(5, 20, 2):
+#     print(c)
+
+# for d in range(20,1,-5): # starting at 20, ending at 1, decrement by 5
+#     print(d)
+
+# books=["HTML", "CSS", "JavaScript", "Python", "Django"]
+# for e in books:
+#     print(e)
+
+# for f in range(10):
+#         if  f==5:
+#             break;
+#         print(f)
+
+# while loop
+
+g = 10
+while g>0:
+    print(g)
+    g = g-1
+
